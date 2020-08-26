6ba7182cac684509ced1dd1b22d0bf1acfeb5f09
Frank Schüler <Frank.Schueler@fiduciagad.de>
2020-08-26 17:35:32 +0200

    update

--- src/fields/wrapper/index.js
+++ src/fields/wrapper/index.js
@@ -2,8 +2,9 @@ import Text from "./Text";
 import Checkbox from "./Checkbox";
 import Radio from "./Radio";
 import File from "./File";
+import Select from "./Select";
 
-export const wrappers = [Text, Checkbox, Radio, File];
+export const wrappers = [Text, Checkbox, Radio, File, Select];
 
 export const findWrapper = (field) => {
 	for (let wrapper of wrappers) {
