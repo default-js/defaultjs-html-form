7481d765696cbc27e249d0574ae4bcc4bcf28138
Frank Schüler <schuelerfrank@gmx.de>
2022-12-01 22:36:25 +0100

    update editable handle

--- src/handels/EditableHandle.js
+++ src/handels/EditableHandle.js
@@ -1,36 +1,31 @@
-import {
-ATTRIBUTE_EDITABLE_CONDITION
-} from "../Constants";
+import { ATTRIBUTE_EDITABLE_CONDITION } from "../Constants";
+import { ExpressionResolver } from "@default-js/defaultjs-expression-language";
 
-class EditableHandle{
+class EditableHandle {
+	#initialized = false;
+	#base;
+	#condition;
 
-    #initialized = false;
-    #base;
-    #condition;
+	constructor(base) {
+		this.#base = base;
+	}
 
-    constructor(base){  
-        this.#base = base;
-    }
+	get condition() {
+		if (!this.#initialized) {
+			this.#condition = this.#base.attr(ATTRIBUTE_EDITABLE_CONDITION) || "";
+			this.#initialized = true;
+		}
 
-    get condition(){
-        if(!this.#initialized){
-            this.#condition = this.#base.attr(ATTRIBUTE_EDITABLE_CONDITION) || "";
-            this.#initialized = true;
-        }
+		return this.#condition;
+	}
 
-        return this.#condition;
-    }
+	async validate(data) {
+		const current = this.#base.editable;
+		const editable = this.condition ? await ExpressionResolver.resolve(this.condition, data, false) : true;
+		if (editable != current) this.#base.editable = editable;
 
-    async validate(data){
-        const current = this.#base.editable;                 
-        const editable = this.condition ? await ExpressionResolver.resolve(this.condition, data, false) : true;
-        if(editable != current)
-            this.#base.editable = editable;
-
-        return editable;
-    }
-
-};
+		return editable;
+	}
+}
 
 export default EditableHandle;
-
